2fdfa007a5e5af81d69171482c2224bc2b64ea69
sigtheidiot <sigridge@stud.ntnu.no>
2020-10-17 13:08:02 +0200

    Add on hover functionality

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -3,8 +3,10 @@ import "../../styles/header.css";
 
 function Header() {
   return (
-    <div>
+    <div id="headerContainer">
       <h1>Spicy sitater fra Krydderhyllen</h1>
+      <button className="button submitt">Har noen sakt noe dumt?</button>
+      <button className="button random">Vil du ha et tilfeldig sitat?</button>
     </div>
   );
 }
