c0519bc3d23cde2524e5baa98186823217a5f72a
Karolill <56150049+Karolill@users.noreply.github.com>
2021-10-01 23:49:19 +0200

    Changed design on add quote page

--- src/components/atoms/AddQuoteForm.tsx
+++ src/components/atoms/AddQuoteForm.tsx
@@ -2,31 +2,35 @@ import React, { useState } from "react";
 import { quotesRef } from "../../firebaseSetup";
 import { QuoteProps } from "../../models/IQuoteProps";
 
-
 export const AddQuoteForm = () => {
-  
-const [content, setContent] = useState("");
-const [quoted, setQuoted] = useState("");
-const [author, setAuthor] = useState("Anonymous");
-const [context, setContext] = useState("");
+  const [content, setContent] = useState("");
+  const [quoted, setQuoted] = useState("");
+  const [author, setAuthor] = useState("Anonymous");
+  const [context, setContext] = useState("");
 
-const submitForm = (e: React.FormEvent<EventTarget>) => {
-  const Quote : QuoteProps = {
-    content: content,
-    quoted: quoted,
-    author: author,
-    context: context || undefined,
-    timestamp: Date().valueOf()
-  }
-  quotesRef.push(Quote);
-  console.log("You really hoped this was submitted? :lolz:");
-};
+  const submitForm = (e: React.FormEvent<EventTarget>) => {
+    const Quote: QuoteProps = {
+      content: content,
+      quoted: quoted,
+      author: author,
+      context: context || undefined,
+      timestamp: Date().valueOf(),
+    };
+    quotesRef.push(Quote);
+    console.log("You really hoped this was submitted? :lolz:");
+  };
   return (
     <div className="add-quotes">
       <h2>Document a new (and spicy?) quote below!</h2>
       <form onSubmit={submitForm}>
         <label>Quoted*</label>
-        <input name="quoted" type="text" placeholder="Karoline Sæbø" required onChange={(e) => setQuoted(e.target.value)}/>
+        <input
+          name="quoted"
+          type="text"
+          placeholder="Karoline Sæbø"
+          required
+          onChange={(e) => setQuoted(e.target.value)}
+        />
 
         <label>Content*</label>
         <input
@@ -45,7 +49,7 @@ const submitForm = (e: React.FormEvent<EventTarget>) => {
           onChange={(e) => setContext(e.target.value)}
         />
 
-        <input type="submit" value="Add quote" />
+        <input type="submit" className="button" value="Add quote" />
       </form>
     </div>
   );
